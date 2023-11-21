2d570b50c2c8a7130d57666019e3d0577648d2a8
dev-wjk <jake1101dev@gmail.com>
2023-11-21 16:39:11 +0900

    complete survey form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
 export default {
   content: [
     "./index.html",
@@ -21,5 +22,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [forms()],
 };
