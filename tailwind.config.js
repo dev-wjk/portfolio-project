d1043898b2b03775710b67acc2006722dc627a69
dev-wjk <jake1101dev@gmail.com>
2023-12-01 04:40:15 +0900

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,23 +10,19 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        primary: "#334155",
-        secondary: "#94a3b8",
-        tertiary: "#f8fafc",
-        emphasis: "",
-        "gradient-from": "#06b6d4",
-        "gradient-to": "#14b8a6",
-      },
-      backgroundColor: {
-        primary: "#e2e8f0",
-      },
       keyframes: {
         blink: {
           "0%": { opacity: 1 },
           "25%": { opacity: 0 },
           "50%": { opacity: 1 },
           "75%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        blink2: {
+          "0%": { opacity: 1 },
+          "25%": { opacity: 0.3 },
+          "50%": { opacity: 1 },
+          "75%": { opacity: 0.3 },
           "100%": { opacity: 1 },
         },
         expand: {
@@ -45,6 +41,8 @@ export default {
       animation: {
         blink:
           "blink 1.75s cubic-bezier(0.175, 0.885, 0.32, 1.275) infinite both",
+        blink2:
+          "blink2 1.75s cubic-bezier(0.175, 0.885, 0.32, 1.275) infinite both",
         expand: "expand 0.3s cubic-bezier(0.215, 0.610, 0.355, 1.000) both",
       },
     },
