f3476a0cea3ae571097f478084999bb2f05dcd2c
dev-wjk <jake1101dev@gmail.com>
2023-12-01 05:14:48 +0900

    add transition

--- src/main.ts
+++ src/main.ts
@@ -1,104 +1,64 @@
-// function addEventsForMobile() {
-//   document.body.addEventListener("click", (ev: MouseEvent) => {
-//     if (ev.target instanceof Element) {
-//       menuBtnClickHandler(ev.target);
-//       projectsBtnClickHandler(ev.target);
-//     }
-//   });
-
-//   function menuBtnClickHandler(target: Element) {
-//     const btn = document.querySelector(".mobile-menu-btn")!;
-//     const nav = document.querySelector("nav")!;
-
-//     if (btn.isEqualNode(target)) {
-//       if (btn.classList.contains("fa-bars")) {
-//         btn.classList.remove("fa-bars");
-//         btn.classList.add("fa-xmark");
-//       } else {
-//         btn.classList.remove("fa-xmark");
-//         btn.classList.add("fa-bars");
-//       }
-
-//       nav.classList.toggle("hidden");
-//     }
-
-//     if (!btn.isEqualNode(target) && btn.classList.contains("fa-xmark")) {
-//       btn.classList.remove("fa-xmark");
-//       btn.classList.add("fa-bars");
-//       nav.classList.toggle("hidden");
-//     }
-//   }
-
-//   function projectsBtnClickHandler(target: Element) {
-//     const HIDE_PROJECTS = "hidden";
-//     const SHOW_ALL = "Show All";
-//     const CLOSE = "Close";
-//     const btn = document.querySelector(".mobile-projects-btn")!;
-//     const cardList = document.querySelectorAll(".card")!;
-
-//     if (btn.isEqualNode(target)) {
-//       cardList.forEach((c, i) => {
-//         if (i > 0) {
-//           c.classList.toggle(HIDE_PROJECTS);
-//         }
-//       });
-
-//       btn.textContent?.trim() === CLOSE
-//         ? (btn.textContent = SHOW_ALL)
-//         : (btn.textContent = CLOSE);
-//     }
-//   }
-// }
-
-// function toggleHeaderVisibility() {
-//   const HIDE_HEADER = "-translate-y-full";
-//   const header = document.querySelector("header")!;
-//   const linkList = document.querySelectorAll(".nav-link");
-
-//   let scrollByLink = false;
-//   let before = 0;
-//   document.addEventListener("scroll", () => {
-//     const y = document.body.getBoundingClientRect().y;
-//     if (before < y) {
-//       // scroll up;
-//       header.classList.remove(HIDE_HEADER);
-//     }
-
-//     if (before > y) {
-//       // scroll down;
-//       header.classList.add(HIDE_HEADER);
-//     }
-
-//     if (scrollByLink) {
-//       header.classList.add(HIDE_HEADER);
-//     }
-
-//     if (y === 0) {
-//       header.classList.remove(HIDE_HEADER);
-//     }
-
-//     before = y;
-//   });
-
-//   document.addEventListener("scrollend", () => (scrollByLink = false));
-
-//   linkList.forEach((el) => {
-//     el.addEventListener("click", () => {
-//       if (before !== 0) {
-//         header.classList.add(HIDE_HEADER);
-//         scrollByLink = true;
-//       }
-//     });
-//   });
-// }
-
-// addEventsForMobile();
-// toggleHeaderVisibility();
-
 type TypingData = {
   colData: string[];
   emphasis: number[];
 }[][];
+
+function colorSchemeControl() {
+  const IS_ACTIVE = "js-is-active";
+  const container = document.querySelector(".js-color-scheme");
+  const sun = document.querySelector(".fa-sun");
+  const moon = document.querySelector(".fa-moon");
+  const elements = document.querySelectorAll("[class*='dark:']");
+  const classList: string[][] = Array.from(elements).map((el) =>
+    Array.from(el.classList).filter((c) => c.includes("dark:"))
+  );
+
+  container?.addEventListener("click", (ev) => {
+    if (ev?.target instanceof Element) {
+      if (ev.target.classList.contains("fa-sun")) {
+        sun?.classList.add(IS_ACTIVE);
+        moon?.classList.remove(IS_ACTIVE);
+        elements.forEach((el, i) => el.classList.remove(...classList[i]));
+        return;
+      }
+
+      if (ev.target.classList.contains("fa-moon")) {
+        moon?.classList.add(IS_ACTIVE);
+        sun?.classList.remove(IS_ACTIVE);
+        elements.forEach((el, i) => el.classList.add(...classList[i]));
+        return;
+      }
+    }
+  });
+
+  window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)")
+    ? moon?.classList.add(IS_ACTIVE)
+    : sun?.classList.add(IS_ACTIVE);
+}
+
+function addTransition() {
+  const IS_SHOWN = "js-is-shown";
+  const sectionList = document.querySelectorAll(
+    "main > section > div > article"
+  );
+  sectionList.forEach((el) =>
+    el.classList.add("transition", "duration-[1.25s]", "opacity-0")
+  );
+
+  const obs = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(IS_SHOWN);
+        }
+      });
+    },
+    { threshold: 0.9 }
+  );
+
+  sectionList.forEach((el) => obs.observe(el));
+  console.log(sectionList);
+}
 
 function addEventsForMobile() {
   const desc = document.querySelector(".js-header-block-desc");
@@ -147,43 +107,11 @@ function addTypingEffect() {
 
 function addEfectsToProjects() {}
 
-function colorSchemeControl() {
-  const IS_ACTIVE = "js-is-active";
-  const container = document.querySelector(".js-color-scheme");
-  const sun = document.querySelector(".fa-sun");
-  const moon = document.querySelector(".fa-moon");
-  const elements = document.querySelectorAll("[class*='dark:']");
-  const classList: string[][] = Array.from(elements).map((el) =>
-    Array.from(el.classList).filter((c) => c.includes("dark:"))
-  );
-
-  container?.addEventListener("click", (ev) => {
-    if (ev?.target instanceof Element) {
-      if (ev.target.classList.contains("fa-sun")) {
-        sun?.classList.add(IS_ACTIVE);
-        moon?.classList.remove(IS_ACTIVE);
-        elements.forEach((el, i) => el.classList.remove(...classList[i]));
-        return;
-      }
-
-      if (ev.target.classList.contains("fa-moon")) {
-        moon?.classList.add(IS_ACTIVE);
-        sun?.classList.remove(IS_ACTIVE);
-        elements.forEach((el, i) => el.classList.add(...classList[i]));
-        return;
-      }
-    }
-  });
-
-  window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)")
-    ? moon?.classList.add(IS_ACTIVE)
-    : sun?.classList.add(IS_ACTIVE);
-}
-
+colorSchemeControl();
+addTransition();
 addEventsForMobile();
 addTypingEffect();
 addEfectsToProjects();
-colorSchemeControl();
 
 function typingEffect(container: Element, typingData: TypingData) {
   const ROW_CLASS = "js-typing-row";
