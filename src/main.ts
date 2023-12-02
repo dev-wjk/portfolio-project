4280db867811c261245d196d62c84dbe0ad6aaa5
dev-wjk <jake1101dev@gmail.com>
2023-12-02 11:10:19 +0900

    undo

--- src/main.ts
+++ src/main.ts
@@ -27,16 +27,6 @@ function colorSchemeControl() {
     }
   });
 
-  if (
-    localStorage.getItem('color-theme') === 'dark' ||
-    (!('color-theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
-
   window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')
     ? moon?.classList.add(IS_ACTIVE)
     : sun?.classList.add(IS_ACTIVE);
